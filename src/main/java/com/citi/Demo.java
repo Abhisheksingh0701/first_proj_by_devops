f9c99f1d66db38989e18a2133f68224d1faa6ae3
Abhisheksingh0701 <89239889+Abhisheksingh0701@users.noreply.github.com>
2023-09-24 09:58:00 +0530

    Update Demo.java

--- src/main/java/com/citi/Demo.java
+++ src/main/java/com/citi/Demo.java
@@ -9,5 +9,6 @@ public class Demo
     public static void main( String[] args )
     {
         System.out.println( "Hello World! its 2nd Java file" );
+         System.out.println( "Edited By GitHub" );
     }
 }
